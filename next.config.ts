4e7468313b88efc41b75b1e9a6a34c07e10f9a98
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-11-11 07:05:16 +0000

    https://minimum-purple-royb13jjzh.edgeone.app/gettyimages-2075082760-612

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minimum-purple-royb13jjzh.edgeone.app',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
