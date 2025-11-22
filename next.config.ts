c766faf569a74b807650a8268e629f6c4b3268ff
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-11-22 02:54:16 +0000

    Photo video ke liy storege ko upgrade kro

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {googleCloudStorage} from 'genkit/plugins/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(), googleCloudStorage()],
   model: 'googleai/gemini-2.5-flash',
 });
