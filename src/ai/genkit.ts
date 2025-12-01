cd377944a43f3e90fc99c9c9bb893939526c7c11
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-12-01 07:47:41 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {googleCloudStorage} from '@genkit-ai/google-cloud';
+import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI(), googleCloudStorage],
+  plugins: [googleAI(), googleCloud()],
   model: 'googleai/gemini-2.5-flash',
 });
