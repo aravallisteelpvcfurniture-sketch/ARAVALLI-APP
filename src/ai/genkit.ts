3281a5b36210ff3fe3e24723c3240ae704952d54
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-12-02 05:15:22 +0000

    Try fixing this error: `Runtime Error: TypeError: (0 , {imported module

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,9 +2,8 @@
 
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI(), googleCloud()],
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
 });
